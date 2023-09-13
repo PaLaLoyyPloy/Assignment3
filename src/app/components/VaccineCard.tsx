2ce20cfeeca4f734e6d8ba2e6839ea5e3e45e162
PaLaLoyyPloy <Ployza.jlo@gmail.com>
2023-09-13 21:28:59 +0700

    Assignment for Week 5

--- src/app/components/VaccineCard.tsx
+++ src/app/components/VaccineCard.tsx
@@ -1,9 +1,8 @@
-import styles from './vaccinecard.module.css'
 import Image from 'next/image'
+import InteractiveCard from './InteractiveCard';
 export default function VaccineCard ({hospitalName,imgSrc}:{hospitalName:string,imgSrc:string}){
     return(
-        <div className="w-1/5 h-[300px] rounded-lg shadow-lg">
-
+        <InteractiveCard contentName={hospitalName}>
             <div className="w-full h-[70%] relative rounded-t-lg">
                 <Image src={imgSrc}
                 alt='Hospital Picture'
@@ -12,6 +11,6 @@ export default function VaccineCard ({hospitalName,imgSrc}:{hospitalName:string,
                 />
             </div>
             <div className="w-full h-[30%] p-[10px]">{hospitalName}</div>
-        </div>
+        </InteractiveCard>
     );
 }
